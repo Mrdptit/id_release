71d9300c39c4c3f5b1d79571cf32475df4fd8835
Nguyễn Chí Thành <chithanh.ptit@gmail.com>
2017-09-27 16:15:41 +0700

    qwewqe
    ưqeweqqwe

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ var Config = {
   "app_ip"	  :   '10.148.0.2',
   "app_port"  :   '8080',
   "secret"	  :   '@!@aKen2002@!@',
+  "android"	  :   'AAAASW_MSsM:APA91bHxC8Ymwqc7owHR04wwhRQOXS5KWGmLvmuPTZkL4IjPBmzrtkvIN6ZZiXz3JwtX1mUzIxFmpFSBk7NX6UUONicErwiBbQ0yUVYMKn6oS4GGmCCXr3HhRmet-Qx3OSXkCKH99Edi',
   "secretAdmin"	  :   '@!@aKen2002Admin@!@'
 };
 module.exports = Config;
