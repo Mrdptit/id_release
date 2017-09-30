59a22d9033a671fb05947bebfc02f55e8a8bec6a
Nguyễn Chí Thành <chithanh.ptit@gmail.com>
2017-09-30 11:31:39 +0700

    123
    123

--- index.js
+++ index.js
@@ -227,6 +227,8 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/listUsers', function(req, res) {
     res.end(JSON.stringify(users, censor));
 });
