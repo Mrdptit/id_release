4b3ae54cfa560ff62fd30580be6b656bfec7a554
Nguyễn Chí Thành <chithanh.ptit@gmail.com>
2017-11-01 17:54:39 +0700

    123
    123

--- index.js
+++ index.js
@@ -37,12 +37,7 @@ var async = require('async');
 var _ = require('lodash');
 var moment = require('moment-timezone');
 
-// var urlParser = bodyParser.urlencoded({ extended: false });
-
-
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
-
+var urlParser = bodyParser.urlencoded({ extended: false });
 
 var configFirebase = {
     apiKey: "AIzaSyAmYRokQALuWuM53U3O2n2d58N3vdml8uc",
