18a52af5d40fd5c5e9104e5611b1c620c3464769
Nguyễn Chí Thành <chithanh.ptit@gmail.com>
2017-11-01 10:58:35 +0700

    123
    123

--- controllers/admin.js
+++ controllers/admin.js
@@ -26,7 +26,7 @@ imgur.setClientId('7cb30e33649106f');
 imgur.setAPIUrl('https://api.imgur.com/3/');
 
 // parse application/x-www-form-urlencoded
-var urlParser = bodyParser.urlencoded({extended: false});
+var urlParser = bodyParser.urlencoded({ extended: false });
 // parse application/json
 router.use(bodyParser.json());
 
@@ -46,6 +46,7 @@ let transporter = nodemailer.createTransport({
  **********------- MYSQL CONNECT ----*********
  **********--------------------------*********/
 var client;
+
 function startConnection() {
     console.error('CONNECTING');
     client = mysql.createConnection({
@@ -54,7 +55,7 @@ function startConnection() {
         password: config.mysql_pass,
         database: config.mysql_data
     });
-    client.connect(function (err) {
+    client.connect(function(err) {
         if (err) {
             console.error('CONNECT FAILED USERS', err.code);
             startConnection();
@@ -62,20 +63,20 @@ function startConnection() {
             console.error('CONNECTED USERS');
         }
     });
-    client.on('error', function (err) {
+    client.on('error', function(err) {
         if (err.fatal)
             startConnection();
     });
 }
 startConnection();
-client.query("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci", function (error, results, fields) {
+client.query("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci", function(error, results, fields) {
     if (error) {
         console.log(error);
     } else {
         console.log("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
     }
 });
-client.query("SET CHARACTER SET utf8mb4", function (error, results, fields) {
+client.query("SET CHARACTER SET utf8mb4", function(error, results, fields) {
     if (error) {
         console.log(error);
     } else {
@@ -86,10 +87,57 @@ client.query("SET CHARACTER SET utf8mb4", function (error, results, fields) {
  **********------- FUNCTION ------*********
  **********--------------------------*********/
 
+
+
+router.post('/send', urlParser, function(req, res) {
+    var email = req.body.email || req.query.email || req.params.email;
+    var password = req.body.password || req.query.password || req.params.password;
+    var list = req.body.list || req.query.list || req.params.list;
+    var title = req.body.title || req.query.title || req.params.title;
+    var content = req.body.content || req.query.content || req.params.content;
+
+    var trans = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // secure:true for port 465, secure:false for port 587
+        auth: {
+            user: email,
+            pass: password
+        }
+    });
+
+    var list_email = list.split(",");
+    if (list_email.length > 0) {
+        async.forEachOf(list_email, function(element, i, callback) {
+            var tinnhan = {
+                to: '<' + element + '>',
+                subject: title,
+                html: content
+            };
+            trans.sendMail(tinnhan, (error, info) => {
+                if (error) {
+                    console.log(error.message);
+                } else {
+                    console.log('Server responded with "%s"', info.response);
+                    trans.close();
+                }
+                if (i == list_email.length - 1) {
+                    return res.send(echoResponse(200, "OK", "success", false));
+                }
+            });
+        });
+    } else {
+        return res.send(echoResponse(404, "No Email", "success", false));
+    }
+});
+
+
+
+
 /*********--------SIGNIN----------*********/
-router.post('/signin', urlParser, function (req, res) {
+router.post('/signin', urlParser, function(req, res) {
     var userSQL = "SELECT * FROM `administrator` WHERE `username`='" + req.body.username + "' AND `password`='" + req.body.password + "'";
-    client.query(userSQL, function (error, data, fields) {
+    client.query(userSQL, function(error, data, fields) {
         if (error) {
             console.log(error);
             return res.sendStatus(300);
@@ -101,11 +149,11 @@ router.post('/signin', urlParser, function (req, res) {
                 var data_name = data[0].username;
                 var data_pass = data[0].password;
 
-                var json = {pass: data_pass, current_time: currentTime, id: data_key, username: data_name};
-                var token = jwt.sign(json, config.secretAdmin, {expiresIn: '1h'});
+                var json = { pass: data_pass, current_time: currentTime, id: data_key, username: data_name };
+                var token = jwt.sign(json, config.secretAdmin, { expiresIn: '1h' });
 
                 var dataSQL = "UPDATE `administrator` SET `access_token`='" + token + "' WHERE `username`='" + req.body.username + "' AND `password`='" + req.body.password + "'";
-                client.query(dataSQL, function (eUpdate, dUpdate, fUpdate) {
+                client.query(dataSQL, function(eUpdate, dUpdate, fUpdate) {
                     if (eUpdate) {
                         console.log(eUpdate);
                         return res.sendStatus(300);
@@ -122,19 +170,20 @@ router.post('/signin', urlParser, function (req, res) {
     });
 });
 
+
 /*********--------GET ALL USER----------*********/
-router.get('/type=users', function (req, res) {
+router.get('/type=users', function(req, res) {
     var token = req.body.access_token || req.query.access_token || req.headers['x-access-token'] || req.params.access_token;
     if (token) {
-        jwt.verify(token, config.secretAdmin, function (err, decoded) {
+        jwt.verify(token, config.secretAdmin, function(err, decoded) {
             if (err) {
-                return res.json({success: false, message: 'Failed to authenticate token.'});
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
             } else {
                 ///-----Check nếu tồn tại access_token thì chạy xuống dưới
                 var userSQL = "SELECT `facebook_id` FROM `users` WHERE `facebook_point`='-1' AND `facebook_id` IS NOT NULL";
                 //var dk4 = " AND `facebook_id` NOT IN (SELECT `facebook_id` FROM `facebook_bot`)";
                 // var limit = "LIMIT 10";
-                client.query(userSQL, function (error, data, fields) {
+                client.query(userSQL, function(error, data, fields) {
                     if (error) {
                         console.log(error);
                         return res.sendStatus(300);
@@ -145,8 +194,8 @@ router.get('/type=users', function (req, res) {
                             // async.forEachOf(data, function(element, i, callback){
                             //     client.query("INSERT INTO `facebook_bot`(`name`,`facebook_id`) VALUES ('"+nameBot+"','"+data[i].facebook_id+"')");
                             //     if (i === data.length-1) {
-                                    return res.send(echoResponse(200, data, "success", false));
-                                // }
+                            return res.send(echoResponse(200, data, "success", false));
+                            // }
                             // });
                         } else {
                             return res.send(echoResponse(404, 'No user.', 'success', true));
@@ -167,6 +216,7 @@ router.get('/type=users', function (req, res) {
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 function isBase64(str) {
     try {
         return btoa(atob(str)) == str;
@@ -174,6 +224,7 @@ function isBase64(str) {
         return false;
     }
 }
+
 function echoResponse(status, data, message, error) {
     return JSON.stringify({
         status: status,
@@ -182,6 +233,7 @@ function echoResponse(status, data, message, error) {
         error: error
     });
 }
+
 function echo5Response(status, data, other, message, error) {
     return JSON.stringify({
         status: status,
