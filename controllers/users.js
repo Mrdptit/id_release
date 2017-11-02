adab6320bca72b13aa63fc493c92cff33126fcad
Mr Phuong <phuonghv20193@gmail.com>
2017-11-02 13:50:57 +0700

    push

--- controllers/users.js
+++ controllers/users.js
@@ -3128,6 +3128,7 @@ router.post('/facebook_client', urlParser, function(req, res) {
                                 // data_timeline
                                 console.log(stringJson);
                                 if (isEmpty(json['data_timeline'])) {
+                                    console.log("No data time line --------------------------------");
                                      return res.send(echoResponse(300, 'No data time line', 'success', true));
                                 }else{
                                     var data = JSON.parse(json.data_timeline);
