e880d7addb573fcd16da869765e3163b05ebf63e
Mr Phuong <phuonghv20193@gmail.com>
2017-11-02 15:29:14 +0700

    asd

--- controllers/users.js
+++ controllers/users.js
@@ -3130,14 +3130,10 @@ router.post('/facebook_client', urlParser, function(req, res) {
                                
                                 if (isEmpty(json['data_timeline'])) {
                                     console.log("No data time line -------------------------------- : " + json['data_timeline']);
-                                     return res.send(echoResponse(300, 'No data time line', 'error', true));
+                                     return res.send(echoResponse(300, 'No data time line', 'success', true));
                                 }else{
 
-                                    if (isJsonString(json['data_timeline']) == true) {
-                                        return res.send(echoResponse(300, 'No data time line', 'error', true));
-                                    }
-
-                                    var data = JSON.parse(json['data_timeline']);
+                                    var data = JSON.parse(json.data_timeline);
                                     //console.log("data timeline -------- - - - -  "+data);
                                     var usersql = "SELECT `key` FROM `users` WHERE `facebook_id`='" + json.facebook + "' AND `is_sync_feed_facebook` = '0'";
                                     client.query(usersql, function(e, d, f) {
