e778c566cde4f43c9904dc6ccd68f3ca8d104176
Mr Phuong <phuonghv20193@gmail.com>
2017-11-10 11:50:44 +0700

    cmt

--- Localizable/arab.js
+++ Localizable/arab.js
@@ -1,13 +1,15 @@
 var AR = {
 
 //Message
-  "msg":   'I come from AR'
+  "msg":   'I come from AR',
+  "msg_common_new_message"			:'You have a new message', 
   "msg_common_sent_a_photo"			:' sent a photo', 
   "msg_common_sent_a_emoji"			:' sent a emoji',
   "msg_common_sent_a_gif"			:' sent a gif',
   "msg_common_sent_a_video"			:' sent a video',
   "msg_common_sent_a_file"			:' sent a file',
   "msg_common_changed_background_chat" :' changed background conversation',
-  "msg_common_miss_call" :'Miss call from '
+  "msg_common_miss_call" :'Miss call from ',
+  "msg_common_incomming_call" :'Incomming call from '
 };
 module.exports = AR;
