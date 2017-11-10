fcd57710b83e00dcaf73900ccf0c6ccb26cd6bde
Mr Phuong <phuonghv20193@gmail.com>
2017-11-10 10:37:33 +0700

    cmt

--- controllers/messages.js
+++ controllers/messages.js
@@ -810,5 +810,5 @@ function echoResponse(status, data, message, error) {
 /*********--------------------------*********
  **********------- CONTROLLERS ------*********
  **********--------------------------*********/
-express.use(require('./Localzable'));
+// express.use(require('./Localzable'));
 module.exports = router;
