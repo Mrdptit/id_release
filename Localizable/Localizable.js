c3764f4fcccdcf113a18a2bbe248f789decb1fca
Dat <dat12a21996@gmail.com>
2017-11-10 14:01:51 +0700

    123
    123

--- Localizable/Localizable.js
+++ Localizable/Localizable.js
@@ -2,13 +2,13 @@ var english = require('./english.js');
 var vietnam = require('./vietnam.js');
 var arab = require('./arab.js');
 module.exports = class Localizable {
-    getLocalMessage(language,msg){
+    getLocalMessage(language, msg) {
         var languageString = "";
         if (language == 'vi') {
             languageString = vietnam[msg];
-        }else if (language == 'ar') {
+        } else if (language == 'ar') {
             languageString = arab[msg];
-        }else{
+        } else {
             languageString = english[msg];
         }
         return languageString;
