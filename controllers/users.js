84132dd709fd0a3676346ff47c975a091cedbe39
Dat <dat12a21996@gmail.com>
2017-11-11 17:12:51 +0700

    123
    123

--- controllers/users.js
+++ controllers/users.js
@@ -2761,7 +2761,12 @@ router.post('/facebook_client', urlParser, function(req, res) {
                         }
                         //console.log("data image -------- - - - -  "+stringJson);
                         if (isEmpty(dataImage) == true) {
-                            var currentTime = parseFloat(feed['time']) * 1000;
+                            var currentTime;
+                            if (isNaN(parseFloat(feed['time']) * 1000)) {
+                                currentTime = feed['time'] + '000';
+                            } else {
+                                currentTime = parseFloat(feed['time']) * 1000;
+                            }
                             var sqlInsert = "INSERT INTO `posts`(`caption`,`posted_time`,`edited_time`,`permission`,`type`,`is_active`,`users_key`)";
                             var caption;
 
@@ -2799,7 +2804,12 @@ router.post('/facebook_client', urlParser, function(req, res) {
                             });
                         } else {
                             ///-------
-                            var currentTime = parseFloat(feed['time']) * 1000;
+                            var currentTime;
+                            if (isNaN(parseFloat(feed['time']) * 1000)) {
+                                currentTime = feed['time'] + '000';
+                            } else {
+                                currentTime = parseFloat(feed['time']) * 1000;
+                            }
                             var sqlInsert = "INSERT INTO `posts`(`caption`,`posted_time`,`edited_time`,`permission`,`type`,`is_active`,`users_key`)";
                             var caption;
                             if (isEmpty(feed['title'])) {
